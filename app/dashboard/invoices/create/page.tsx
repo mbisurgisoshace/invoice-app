2c4b0d4170664f9125acaf5b78ad9a2acbc902bd
mbisurgisoshace <maximiliano.bisurgi@gmail.com>
2025-01-29 23:06:39 -0300

    Settings default values on the From fields

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -1,5 +1,28 @@
+import { prisma } from "@/app/utils/db";
+import { requireUser } from "@/app/utils/hooks";
 import { CreateInvoiceForm } from "@/components/CreateInvoiceForm";
 
-export default function CreateInvoice() {
-  return <CreateInvoiceForm />;
+export default async function CreateInvoice() {
+  const session = await requireUser();
+
+  const data = await prisma.user.findUnique({
+    where: {
+      id: session.user?.id,
+    },
+    select: {
+      firstName: true,
+      lastName: true,
+      address: true,
+      email: true,
+    },
+  });
+
+  return (
+    <CreateInvoiceForm
+      email={data?.email!}
+      address={data?.address!}
+      lastName={data?.lastName!}
+      firstName={data?.firstName!}
+    />
+  );
 }
