0f7b490f9052f76da19e60a493d904793bf59d97
mbisurgisoshace <maximiliano.bisurgi@gmail.com>
2025-01-31 00:27:25 -0300

    Change some titles on the dashboard

--- components/DashboardBlocks.tsx
+++ components/DashboardBlocks.tsx
@@ -85,13 +85,15 @@ export async function DashboardBlocks() {
 
       <Card>
         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-          <CardTitle className="text-sm font-medium">Open Invoices</CardTitle>
+          <CardTitle className="text-sm font-medium">
+            Pending Invoices
+          </CardTitle>
           <ActivityIcon className="size-4 text-muted-foreground" />
         </CardHeader>
         <CardContent>
           <h2 className="text-2xl font-bold">{`+${pendingInvoices.length}`}</h2>
           <p className="text-xs text-muted-foreground">
-            Invoices which havent been paid
+            Invoices which are currently pending
           </p>
         </CardContent>
       </Card>
