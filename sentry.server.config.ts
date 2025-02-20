d646361b24567b9d055ec50fffd54e9a28301336
mbisurgisoshace <maximiliano.bisurgi@gmail.com>
2025-02-19 23:48:49 -0300

    Setting sentry for user reports and fetching user feedback

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://e2e0d2dc9718d44ffad9ea0e8259a908@o4508849183653888.ingest.us.sentry.io/4508849186996224",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
